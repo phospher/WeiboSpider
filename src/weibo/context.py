9b9f41dbbe642a08f14e9648616d49e7f0b19e88
phospher <phospher@qq.com>
2013-01-12 18:27:53 +0800

    context.py

---
+++ src/weibo/context.py
@@ -0,0 +1,28 @@
+from weibo import UserModel
+
+class RedisContext(object):
+    
+    CONNECTION_POOL = None
+    HOST = 'localhost'
+    PORT = 6379
+    
+    USER_LIST_KEY = 'weibospider_user_list'
+    USER_SET_KEY = 'weibospider_user_set'
+    
+    def __init__(self, redisModule):
+        if not RedisContext.CONNECTION_POOL:
+            RedisContext.CONNECTION_POOL = redisModule.ConnectionPool(host=RedisContext.HOST, port=RedisContext.PORT)
+        self.redisClient = redisModule.Redis(connection_pool=RedisContext.CONNECTION_POOL)
+    
+    def dequeueUser(self):
+        result = UserModel()
+        result.name = self.redisClient.lpop()
+        return result
+    
+    def existsUser(self, userName):
+        return self.redisClient.sismember(RedisContext.USER_SET_KEY, userName)
+    
+    def enqueueUsers(self, userList):
+        for user in userList:
+            if self.redisClient.sadd(RedisContext.USER_SET_KEY, user.name) > 0:
+                self.redisClient.rpush(RedisContext.USER_LIST_KEY, user.name)
---
+++ tests/contextTest.py
@@ -0,0 +1,71 @@
+from unittest import TestCase
+import sys
+from mock import Mock
+
+sys.path.append('../src/')
+
+from weibo.context import RedisContext
+from weibo import UserModel
+
+class RedisModuleMock(object):
+    
+    def __init__(self, redisMock):
+        self._redisMock = redisMock
+    
+    def ConnectionPool(self, *args, **kwargs):
+        return None
+    
+    def Redis(self, *args, **kwargs):
+        return self._redisMock
+
+class RedisContextTest(TestCase):
+    def _dequeueUserInitialize(self):
+        redisMock = Mock()
+        redisMock.lpop = Mock(return_value='test_user')
+        target = RedisContext(RedisModuleMock(redisMock))
+        return target.dequeueUser()
+    
+    def test_dequeueUserReturnUserModelObject(self):
+        actual = self._dequeueUserInitialize()
+        self.assertIsInstance(actual, UserModel)
+    
+    def test_dequeueUserReturnCorrectUserName(self):
+        actual = self._dequeueUserInitialize()
+        self.assertEqual(actual.name, 'test_user')
+    
+    def test_existsUserReturnCorrectResult(self):
+        redisMock = Mock()
+        redisMock.sismember = Mock(side_effect=lambda k, v:True if v == 'test_user' else False)
+        target = RedisContext(RedisModuleMock(redisMock))
+        self.assertTrue(target.existsUser('test_user'))
+    
+    def _enqueueUsersTest(self, assertMethod):
+        redisMock = Mock()
+        redisMock.sadd = Mock(side_effect=lambda k, v:0 if v == 'test_user2' else 1)
+        userBuff = []
+        redisMock.rpush = Mock()
+        
+        param1 = []
+        for i in range(0, 3):
+            user = UserModel()
+            user.name = 'test_user%s' % i
+            param1.append(user)
+        target = RedisContext(RedisModuleMock(redisMock))
+        target.enqueueUsers(param1)
+        
+        assertMethod(redisMock)
+    
+    def test_enqueueUsersInsertTwoUsers(self):
+        def assertRpushCallTwice(redisMock):
+            self.assertEqual(redisMock.rpush.call_count, 2)
+        
+        self._enqueueUsersTest(assertRpushCallTwice)
+    
+    def test_enqueueUsersTestUser0Inserted(self):
+        def assertTestUser1Inserted(redisMock):
+            user0List = [x for x in redisMock.rpush.call_args_list if x[0][1] == 'test_user0']
+            self.assertEqual(len(user0List), 1)
+        
+        self._enqueueUsersTest(assertTestUser1Inserted)
+        
+
