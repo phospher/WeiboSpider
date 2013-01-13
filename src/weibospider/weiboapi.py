52f5b9035e491aa916c9fa59bb924260a77c5e4f
phospher <phospher@qq.com>
2013-01-13 18:04:12 +0800

    sina weibo auto login

--- src/weibospider/weiboapi.py
+++ src/weibospider/weiboapi.py
@@ -1,3 +1,23 @@
+import re
+
+def sinaWeiboAutoAuth(apiClient, userName, password, virtualBrowser):
+    url = apiClient.get_authorize_url()
+    
+    #Open authorize and complete the authorization automatically
+    virtualBrowser.open(url)
+    virtualBrowser.select_form(name='authZForm')
+    virtualBrowser['userId'] = userName
+    virtualBrowser['passwd'] = password
+    virtualBrowser.submit()
+    
+    #Get the authorization code for requesting access token
+    redirectUrl = virtualBrowser.response().geturl()
+    searchResult = re.search(r'[?&]code=(\S+)', redirectUrl)
+    code = searchResult.group(1)
+    
+    request = apiClient.request_access_token(code)
+    apiClient.set_access_token(request.access_token, request.expires_in)
+
 class SinaWeiboAPI(object):
     def __init__(self, weiboAPIModule):
         pass
